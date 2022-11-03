44f134cd920bb505bd575c48d82486b6dc3fe93b
qianl <qianl@educ.college-montreal.qc.ca>
2022-11-03 16:16:17 -0400

    comment trouver diviseur de 3

--- TP3.py
+++ TP3.py
@@ -1,3 +1,8 @@
+"""
+Programme fait par Lily Qian
+Groupe: 403
+Description: Combat des monstres
+"""
 import random
 
 hp = 20
@@ -6,38 +11,30 @@ monster_count = 0
 combat_count = 0
 victory_count = 0
 defeat_count = 0
-
-
-def total_monster_count():
-    global monster_count
-    monster_count += 1
-
-
-def total_combat_count():
-    global combat_count
-    combat_count += 1
-
-
+win_streak = 0
 
 def defeat_conditions():
-    global hp
-    global defeat_count
+    global hp, defeat_count
     hp -= random_monster_strength
     defeat_count += 1
 
+
 def victory_conditions():
-    global hp
-    global victory_count
-    global defeated_monster_count
+    global hp, victory_count, defeated_monster_count, win_streak
     hp += random_monster_strength
     victory_count += 1
+    win_streak += 1
     defeated_monster_count += 1
+
 
 
 jeu = True
 
 while jeu:
-    random_monster_strength = random.randint(1, 5) + random.randint(1, 5)
+    if win_streak == 3 and win_streak % 3:
+        random_monster_strength = random.randint(8, 10) + random.randint(8, 10)
+    else:
+        random_monster_strength = random.randint(1, 5) + random.randint(1, 5)
     print(f"Vous tombez face avec un adversaire de difficulté : {random_monster_strength}")
     choix = int(input("Que voulez_vous faire?: "
                       "\n1- Combattre cet adversaire "
@@ -46,10 +43,10 @@ while jeu:
                       "\n4- Quitter la partie "
                       "\nVotre option:"))
     if choix == 1:
-        total_monster_count()
-        total_combat_count()
+        monster_count += 1
+        combat_count += 1
 
-        random_player_strength = random.randint(1, 2) + random.randint(1, 2)
+        random_player_strength = random.randint(1, 6) + random.randint(1, 6)
         print(f"Adversaire : {monster_count}"
               f"\nForce de l'adversaire : {random_monster_strength}"
               f"\nNiveau de vie de l'usager : {hp}"
@@ -59,22 +56,27 @@ while jeu:
             print("Dernier combat = défaite ")
             defeat_conditions()
             print(f"Niveau de vie: {hp}")
+            win_streak = 0
             if hp > 0:
                 jeu = True
             if hp <= 0:
                 print(f"La partie est terminée. vous avez tué {defeated_monster_count} monstres")
                 jeu = False
-
         elif random_monster_strength < random_player_strength:
             print("Dernier combat = victoire")
             victory_conditions()
             print(f"Niveau de vie: {hp}")
             jeu = True
 
-
-
     elif choix == 2:
-        print("choix 2")
+        hp -= 1
+        print("Vous avez perdu 1 vie."
+              f"\nNiveau de vie: {hp}")
+        if hp > 0:
+            jeu = True
+        if hp <= 0:
+            print(f"La partie est terminée. vous avez tué {defeated_monster_count} monstres")
+            jeu = False
 
     elif choix == 3:
         print("Pour réussir un combat, il faut que la valeur du dé lancé soit"
@@ -82,8 +84,13 @@ while jeu:
               "\nde vie de l'usager est augmenté de la force de l'adversaire."
               "\nde lune défaite a lieu lorsque la valeur du dé lancé par l'usager"
               "\nest inférieure ou égale à la force de l'adversaire. Dans ce cas,"
-              "\n")
-
+              "\nle niveau de vie de l'usager est diminué de la force de l'adversaire")
+        decision = str(input("Voulez-vous continuer le jeu? o/n"))
+        if decision == "o":
+            jeu = True
+        elif decision == "n":
+            print("Merci et au revoir...")
+            jeu = False
 
     elif choix == 4:
         print("Merci et au revoir...")
